7caf06e017f936a6ac25a10ce93b07824279d44d
Victor Torres <vpaivatorres@gmail.com>
2015-10-21 21:05:23 -0300

    Closes #4

--- src/mdterm.py
+++ src/mdterm.py
@@ -1,8 +1,15 @@
 #coding: utf-8
 import sys
 
-def main(argv):
+class MarkdownParser(object):
     """mdterm - prints markdown in your terminal"""
+
+    def __init__(self, file):
+        # Reads file and store its raw data
+        self.raw = file.read()
+
+
+def main(argv):
     # Verifies if argument number is correct
     if len(argv) != 1:
         print 'Usage: mdterm file...'
@@ -10,13 +17,13 @@ def main(argv):
         
     # Tries to open and read file
     try:
-        markdown_file = open(argv[0]).read()
+        markdown_file = open(argv[0])
     except:
         print 'Could not open file.'
         return
 
-    # Prints file
-    print markdown_file
+    markdown_parser = MarkdownParser(markdown_file)
+    print markdown_parser.raw
 
 if __name__ == "__main__":
     main(sys.argv[1:])
