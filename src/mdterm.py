df451918fb73cc94076575499cc60ea038fcc35e
Victor Torres <vpaivatorres@gmail.com>
2015-10-21 22:28:03 -0300

    Parsing headers.

--- src/mdterm.py
+++ src/mdterm.py
@@ -1,33 +1,63 @@
 #coding: utf-8
 import sys
+from colored import fg, attr
 from markdown2 import Markdown
 from HTMLParser import HTMLParser
+
+DEFAULT_THEME = dict(
+    headers=dict(
+        h1='215',
+        h2='172',
+        h3='166',
+        h4='160',
+        h5='88',
+        h6='52'
+    )
+)
 
 class MarkdownParser(HTMLParser):
     """mdterm - prints markdown in your terminal"""
 
     def __init__(self, file):
-        # Executes HTMLParser init method
-        # as it's an 'old-style' class
+        # Executes HTMLParser init method as it's an 'old-style' class
         HTMLParser.__init__(self)
-        # Reads file and store its raw data
+
+        self.raw = ''
+        self.html = ''
+        self.term = ''
+
+        self.theme = DEFAULT_THEME
+
+        self.read_file(file)
+        self.parse_markdown()
+        self.parse_html()
+
+    def read_file(self, file):
+        """Reads markdown file and stores its raw data."""
         self.raw = file.read()
-        # Parses Markdown into HTML
+
+    def parse_markdown(self):
+        """Parses Markdown into HTML."""
         markdowner = Markdown()
         self.html = markdowner.convert(self.raw)
-        # Parses HTML into Terminal text
-        self.term = ''
+
+    def parse_html(self):
+        """Parses HTML into Terminal text."""
         self.feed(self.html)
+
 
     def handle_starttag(self, tag, attrs):
         """Encountered a start tag"""
         # self.term += tag
-        pass
+        if tag.startswith('h') and tag[1] != 'r':
+            self.term += fg(self.theme['headers']['h%s' % tag[1]])
+            self.term += ' ' * (int(tag[1]) - 1)
 
     def handle_endtag(self, tag):
         """Encountered an end tag"""
         # self.term += tag
-        pass
+        if tag.startswith('h'):
+            self.term += attr('reset')
 
     def handle_data(self, data):
         """Encountered some data"""
@@ -39,7 +69,7 @@ def main(argv):
     if len(argv) != 1:
         print 'Usage: mdterm file...'
         return
-        
+
     # Tries to open and read file
     try:
         markdown_file = open(argv[0])
@@ -49,6 +79,7 @@ def main(argv):
 
     markdown_parser = MarkdownParser(markdown_file)
     print markdown_parser.term
+    markdown_file.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
